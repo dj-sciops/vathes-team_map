97920142390d28627e8dab57653f119d051b8de5
Thinh Nguyen <thinh@vathes.com>
2023-08-30 18:55:41 -0500

    Create delay_response_NWB_export_DANDI_release_Aug2023.py

---
+++ scripts/delay_response_NWB_export_DANDI_release_Aug2023.py
@@ -0,0 +1,25 @@
+"""
+Additional packages required
+
+pip install git+https://github.com/dj-sciops/djsciops-python.git
+pip install --upgrade pyopenssl
+pip install opencv-python
+pip install git+https://github.com/ttngu207/element-interface.git
+
+apt-get install ffmpeg libsm6 libxext6 g++ -y
+"""
+
+from pipeline import publication
+
+
+def main():
+    keys = publication.NWBFileExport.key_source.fetch('KEY')
+
+    key = keys[6]
+
+    publication.NWBFileExport.populate(key)
+    publication.DANDIupload.populate(key)
+
+
+if __name__ == "__main__":
+    main()
