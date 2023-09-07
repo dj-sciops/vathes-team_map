f9de2fd2dda41848249589fdf986eee6d2b3f206
Thinh Nguyen <thinh@vathes.com>
2023-09-07 16:43:57 -0500

    add step to clean up generic errors

--- scripts/delay_response_NWB_export_DANDI_release_Aug2023.py
+++ scripts/delay_response_NWB_export_DANDI_release_Aug2023.py
@@ -17,9 +17,26 @@ populate_settings = dict(reserve_jobs=True, suppress_errors=True)
 
 def main():
     for _ in range(6):
+        _clean_up()
         publication.DANDIupload.populate(max_calls=1, **populate_settings)
         publication.NWBFileExport.populate(max_calls=1, **populate_settings)
         publication.DANDIupload.populate(max_calls=1, **populate_settings)
+
+
+def _clean_up():
+    _generic_errors = [
+        "%Deadlock%",
+        "%Lock wait timeout%",
+        "%MaxRetryError%",
+        "%KeyboardInterrupt%",
+        "InternalError: (1205%",
+        "%SIGTERM%",
+        "%LostConnectionError%",
+    ]
+    (publication.schema.jobs
+     & 'status = "error"'
+     & [f'error_message LIKE "{e}"' for e in _generic_errors]
+     ).delete()
 
 
 if __name__ == "__main__":
